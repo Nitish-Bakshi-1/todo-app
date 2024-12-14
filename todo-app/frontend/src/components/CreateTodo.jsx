4c122e564ce71151d6c97577a54a2d7ad14a0fa2
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-14 20:59:53 +0530

    frontend

---
+++ todo-app/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+export const CreateTodo = () => {
+  return (
+    <div>
+      <input type="text" placeholder="title" /> <br />
+      <input type="text" placeholder="description" /> <br />
+      <button>CreateTodo</button>
+    </div>
+  );
+};
