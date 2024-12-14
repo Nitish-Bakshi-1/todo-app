e8332f75b354fcc1fd29e5d02bef2c5dc00da5ff
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-14 22:03:55 +0530

    frontend

--- todo-app/frontend/src/components/CreateTodo.jsx
+++ todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
 export const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
-      <input type="text" placeholder="title" /> <br />
-      <input type="text" placeholder="description" /> <br />
-      <button>CreateTodo</button>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />{" "}
+      <br />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />{" "}
+      <br />
+      <button
+        onClick={() => {
+          fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-Type": "application/json", // Correct header name
+            },
+          }).then(async (res) => {
+            const json = await res.json();
+            console.log(json);
+          });
+        }}
+      >
+        CreateTodo
+      </button>
     </div>
   );
 };
